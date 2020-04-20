aa32f4b0221d7ae6fefde4663d54bea1dae9aa5b
ShruthiKannurSvmx <shruthi.kannur@servicemax.com>
2020-04-20 18:34:47 +0530

    Updates

---
+++ precache-manifest.b17c72bb70507de15b4152713d19a386.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bef1cd3185be75aec46f165e85674f2",
+    "url": "/Tic-Tac-Toe/index.html"
+  },
+  {
+    "revision": "a062864390fb5f64eb7a",
+    "url": "/Tic-Tac-Toe/static/css/main.8aabbe20.chunk.css"
+  },
+  {
+    "revision": "9c047e207657bb86596b",
+    "url": "/Tic-Tac-Toe/static/js/2.48b7d735.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tic-Tac-Toe/static/js/2.48b7d735.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a062864390fb5f64eb7a",
+    "url": "/Tic-Tac-Toe/static/js/main.0c69823b.chunk.js"
+  },
+  {
+    "revision": "10ba2fbbde4388b88536",
+    "url": "/Tic-Tac-Toe/static/js/runtime-main.1e862f0b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Tic-Tac-Toe/static/media/logo.5d5d9eef.svg"
+  }
+]);
